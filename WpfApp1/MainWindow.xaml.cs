1daeda0ecf9d82cc9507bae06b534effc83a74fa
SokolovYar <sokolov_ya@proton.me>
2023-12-17 18:48:23 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string buttonContent = (sender as Button).Content.ToString();
+
+            switch (buttonContent) 
+            {
+                case "CE":
+                    CalcStr.Text = "";
+                    Rezult.Text = "";
+                    break;
+
+                case "C":
+                    Rezult.Text = "";
+                    break;
+
+                case "<":
+                    if (CalcStr.Text.Length > 1) CalcStr.Text = CalcStr.Text.Remove(CalcStr.Text.Length - 1);
+                    else CalcStr.Text = "";
+                    break;
+
+                case "=":
+                    try
+                    {
+                        Rezult.Text = new DataTable().Compute(CalcStr.Text, "").ToString();
+                    }
+                    catch { Rezult.Text = "ERROR"; }   
+                    break;
+
+                default:
+                    CalcStr.Text += buttonContent;
+                    break;
+            }
+        }
+    }
+}
